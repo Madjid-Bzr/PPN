5502641955c10969461ac3884f98b0769aa4ff8f
Madjid Bzr <madjid.bouzourene@uvsq.fr>
2022-05-05 23:50:58 +0200

    Renommage des fichiers pour plus de lisibilité et modification de Makefile

--- Code Nbody3D/README.md
+++
@@ -1,1 +0,0 @@
-# VersionFinaleNbody3D
--- Code Nbody3D/nbody.c
+++ Code Nbody3D/nbody.c
@@ -117,7 +117,7 @@ int main(int argc, char **argv)
   
   
   //
-  const u64 n = (argc > 1) ? atoll(argv[1]) : 16384;
+  const u64 n = (argc > 1) ? atoll(argv[1]) : 100000;
   const u64 steps= 10;
   const f32 dt = 0.01;
 
