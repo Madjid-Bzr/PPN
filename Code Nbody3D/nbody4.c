64cf9210f43d4ced1f7d5de60cdcfab249e0d5b0
Mehidi Anis <anis.mehidi@se.univ-bejaia.dz>
2022-05-05 22:50:04 +0200

    Modification code Nbody3D Pthread

--- Code Nbody3D/nbody4.c
+++ Code Nbody3D/nbody4.c
@@ -24,17 +24,17 @@ typedef struct srtuct_1
 {
 
   particle_t *p;
-  u64 o;
-  u64 n;
+  u64 old;
+  u64 new;
 
 } srtuct_1;
 
 //
 void *init(void *arg)
 {
-  srtuct_1 *une = (srtuct_1 *)arg;
+  srtuct_1 *particule = (srtuct_1 *)arg;
 
-  for (u64 i = une->o; i < une->o + une->n; i += 4)
+  for (u64 i = particule->old; i < particule->new; i ++)
   {
 
     u64 r1 = (u64)rand();
@@ -42,41 +42,14 @@ void *init(void *arg)
     f32 sign = (r1 > r2) ? 1 : -1;
 
     //
-    une->p->x[i] = sign * (f32)rand() / (f32)RAND_MAX;
-    une->p->y[i] = (f32)rand() / (f32)RAND_MAX;
-    une->p->z[i] = sign * (f32)rand() / (f32)RAND_MAX;
+    particule->p->x[i] = sign * (f32)rand() / (f32)RAND_MAX;
+    particule->p->y[i] = (f32)rand() / (f32)RAND_MAX;
+    particule->p->z[i] = sign * (f32)rand() / (f32)RAND_MAX;
 
     //
-    une->p->vx[i] = (f32)rand() / (f32)RAND_MAX;
-    une->p->vy[i] = sign * (f32)rand() / (f32)RAND_MAX;
-    une->p->vz[i] = (f32)rand() / (f32)RAND_MAX;
-
-    une->p->x[i + 1] = sign * (f32)rand() / (f32)RAND_MAX;
-    une->p->y[i + 1] = (f32)rand() / (f32)RAND_MAX;
-    une->p->z[i + 1] = sign * (f32)rand() / (f32)RAND_MAX;
-
-    //
-    une->p->vx[i + 1] = (f32)rand() / (f32)RAND_MAX;
-    une->p->vy[i + 1] = sign * (f32)rand() / (f32)RAND_MAX;
-    une->p->vz[i + 1] = (f32)rand() / (f32)RAND_MAX;
-
-    une->p->x[i + 2] = sign * (f32)rand() / (f32)RAND_MAX;
-    une->p->y[i + 2] = (f32)rand() / (f32)RAND_MAX;
-    une->p->z[i + 2] = sign * (f32)rand() / (f32)RAND_MAX;
-
-    //
-    une->p->vx[i + 2] = (f32)rand() / (f32)RAND_MAX;
-    une->p->vy[i + 2] = sign * (f32)rand() / (f32)RAND_MAX;
-    une->p->vz[i + 2] = (f32)rand() / (f32)RAND_MAX;
-
-    une->p->x[i + 3] = sign * (f32)rand() / (f32)RAND_MAX;
-    une->p->y[i + 3] = (f32)rand() / (f32)RAND_MAX;
-    une->p->z[i + 3] = sign * (f32)rand() / (f32)RAND_MAX;
-
-    //
-    une->p->vx[i + 3] = (f32)rand() / (f32)RAND_MAX;
-    une->p->vy[i + 3] = sign * (f32)rand() / (f32)RAND_MAX;
-    une->p->vz[i + 3] = (f32)rand() / (f32)RAND_MAX;
+    particule->p->vx[i] = (f32)rand() / (f32)RAND_MAX;
+    particule->p->vy[i] = sign * (f32)rand() / (f32)RAND_MAX;
+    particule->p->vz[i] = (f32)rand() / (f32)RAND_MAX;
   }
 
   pthread_exit(NULL);
@@ -85,31 +58,32 @@ void *init(void *arg)
 //
 void *move_particles(void *arg)
 {
-  srtuct_1 *une = (srtuct_1 *)arg;
+  srtuct_1 *particule = (srtuct_1 *)arg;
   //
   const f32 softening = 1e-20;
 
   //
-  for (u64 i = une->o; i < une->o + une->n; i++)
+  for (u64 i = particule->old; i < particule->new; i++)
   {
     //
     f32 fx = 0.0;
     f32 fy = 0.0;
     f32 fz = 0.0;
-    const f32 p_xi = une->p->x[i];
-    const f32 p_yi = une->p->y[i];
-    const f32 p_zi = une->p->z[i];
+    const f32 p_xi = particule->p->x[i];
+    const f32 p_yi = particule->p->y[i];
+    const f32 p_zi = particule->p->z[i];
     // 23 floating-point operations
-    for (u64 j = une->o; j < une->o + une->n; j++)
+    for (u64 j = particule->old; j < particule->new; j++)
     {
       // Newton's law
-      const f32 dx = une->p->x[j] - p_xi; // 1
-      const f32 dy = une->p->y[j] - p_yi; // 2
-      const f32 dz = une->p->z[j] - p_zi; // 3
+      const f32 dx = particule->p->x[j] - p_xi; // 1
+      const f32 dy = particule->p->y[j] - p_yi; // 2
+      const f32 dz = particule->p->z[j] - p_zi; // 3
       const f32 d_2 = (dz * dz) + softening;
       const f32 d_3 = (dx * dx) + d_2;
       const f32 d_4 = (dy * dy) + d_3;
       const f32 t = sqrtf(d_4);
+
       const f32 d_3_over_2 = 1.0f / (d_4 * t); // 11
 
       // Net force
@@ -119,29 +93,17 @@ void *move_particles(void *arg)
     }
 
     //
-    une->p->vx[i] += 0.01 * fx; // 19
-    une->p->vy[i] += 0.01 * fy; // 21
-    une->p->vz[i] += 0.01 * fz; // 23
+    particule->p->vx[i] += (0.01 * fx); // 19
+    particule->p->vy[i] += (0.01 * fy); // 21
+    particule->p->vz[i] += (0.01 * fz); // 23
   }
 
   // 3 floating-point operations
-  for (u64 i = une->o; i < une->o + une->n; i += 4)
+  for (u64 i = particule->old; i < particule->new; i ++)
   {
-    une->p->x[i] += 0.01 * une->p->vx[i];
-    une->p->y[i] += 0.01 * une->p->vy[i];
-    une->p->z[i] += 0.01 * une->p->vz[i];
-
-    une->p->x[i + 1] += 0.01 * une->p->vx[i + 1];
-    une->p->y[i + 1] += 0.01 * une->p->vy[i + 1];
-    une->p->z[i + 1] += 0.01 * une->p->vz[i + 1];
-
-    une->p->x[i + 2] += 0.01 * une->p->vx[i + 2];
-    une->p->y[i + 2] += 0.01 * une->p->vy[i + 2];
-    une->p->z[i + 2] += 0.01 * une->p->vz[i + 2];
-
-    une->p->x[i + 3] += 0.01 * une->p->vx[i + 3];
-    une->p->y[i + 3] += 0.01 * une->p->vy[i + 3];
-    une->p->z[i + 3] += 0.01 * une->p->vz[i + 3];
+    particule->p->x[i] += 0.01 * particule->p->vx[i];
+    particule->p->y[i] += 0.01 * particule->p->vy[i];
+    particule->p->z[i] += 0.01 * particule->p->vz[i];
   }
 
   pthread_exit(NULL);
@@ -157,7 +119,7 @@ int main(int argc, char **argv)
   srtuct_1 arg1[NB_PTHREADS];
 
   //
-  const u64 n = (argc > 1) ? atoll(argv[1]) : 225536;
+  const u64 n = (argc > 1) ? atoll(argv[1]) : 100000;
   const u64 steps = 10;
 
   //
@@ -179,20 +141,20 @@ int main(int argc, char **argv)
   p->vz = aligned_alloc(64, n * sizeof(f32));
   
 
-  for (u64 l = 0; l < NB_PTHREADS; l++)
+  for (u64 i = 0; i < NB_PTHREADS; i++)
   {
-    arg1[l].p = p;
-    arg1[l].o = (l * n) / NB_PTHREADS;
-    arg1[l].n = n / NB_PTHREADS;
+    arg1[i].p = p;
+    arg1[i].old = (i * n)+1 / NB_PTHREADS;
+    arg1[i].new = n+1 / NB_PTHREADS;
   }
 
-  for (u64 m = 0; m < NB_PTHREADS; m++)
+  for (u64 i = 0; i < NB_PTHREADS; i++)
   {
-    pthread_create(&t1[m], NULL, init, (void *)&arg1[m]);
+    pthread_create(&t1[i], NULL, init, (void *)&arg1[i]);
   }
-  for (u64 m = 0; m < NB_PTHREADS; m++)
+  for (u64 i = 0; i < NB_PTHREADS; i++)
   {
-    pthread_join(t1[m], NULL);
+    pthread_join(t1[i], NULL);
   }
   //
 
@@ -252,7 +214,23 @@ int main(int argc, char **argv)
   printf("\033[1m%s %4s \033[42m%10.1lf +- %.1lf GFLOP/s\033[0m\n",
          "Average performance:", "", rate, drate);
   printf("-----------------------------------------------------\n");
-  
+  f64 somme_x = 0, somme_y = 0, somme_z = 0, somme_vx = 0, somme_vy = 0, somme_vz = 0;
+  for (int i = 0; i < n; i++)
+  {
+
+    somme_x += p->x[i];
+    somme_y += p->y[i];
+    somme_z += p->z[i];
+    somme_vx += p->vx[i];
+    somme_vy += p->vy[i];
+    somme_vz += p->vz[i];
+  }
+  printf("Somme X = %f \n", somme_x);
+  printf("Somme Y = %f \n", somme_y);
+  printf("Somme Z = %f \n", somme_z);
+  printf("Somme VX = %f \n", somme_vx);
+  printf("Somme VY = %f \n", somme_vy);
+  printf("Somme VZ = %f \n", somme_vz);
   //
   free(p->x);
   free(p->y);
